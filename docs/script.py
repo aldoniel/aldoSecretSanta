ffdcf355a91b87997d27e6d8aba8a8901bf8d54f
aldoniel <90577602+aldoniel@users.noreply.github.com>
2025-12-24 03:09:35 +0100

    miroir en ligne commit màj algo comp + css

--- docs/script.py
+++ docs/script.py
@@ -1,6 +1,9 @@
 """
 Le tirage au sort de secret Santa par Aldoniel 
 en interne, tout est en lowercase. la majuscule c'est juste à l'affichage
+todo exporter l'url ap de a et on t pour que ce soit trié
+optimiser compression ac des majuscules
+déboguer l'antitriche la désactivation du lien marche pas
 """
 from browser import document,  html,window #window est l'objet qui donne accès au js global namespace
 from browser.local_storage import storage
@@ -9,8 +12,8 @@ import frenchnamecompressor
 
 
 # global variables
-doc_usr=html.DIV(id="usr")
-doc_adm = html.DIV(id="adm",style="display: none;")
+doc_usr=html.DIV(Class="w3-container",id="usr" )
+doc_adm = html.DIV(Class="w3-container",id="adm",style="display: none;")
 
 
 class Circularcoord():
@@ -19,12 +22,14 @@ class Circularcoord():
     en parcourant une liste par indice comme un tableau circulaire
     start=l'indice de départ, ex 0
     outbound=l'indice de fin, ex len-1
-    attention ne vérifie pas si size=0 ou si start est outbound...
+    attention ne vérifie pas si size=0...
     """
     def __init__(self, start:int, outbound:int) -> None:
         self.i: int = start
         self.outbound: int = outbound
         self.size: int = self.outbound + 1
+        assert start >=0
+        assert outbound>0
 
     def geti(self) -> int:
         i: int = self.i
@@ -37,12 +42,9 @@ def decodeURI() -> list[str]:
    renvoie une liste d'élements décodés après ? de l'url, triés
    """
    a:str=str(window.location.search)
-   a=a.lower()
    if a=="":
        return []
-   #b: list[str]=a[1:].split("&")
    c:list[str]= [window.decodeURIComponent(item) for item in a[1:].split("&") ]
-   c.sort()
    return c
     
 def makeURI(liste)-> str:
@@ -63,11 +65,6 @@ class ToggleMode:
         ToggleMode.tog = not ToggleMode.tog
 
 
-
-
-        
-
-
 class MainClass():
     def __init__(self):
         # vérifier que n'a pas déjà joué
@@ -84,18 +81,18 @@ class MainClass():
 
         self.couplage={}#dict[str,str]
         try:
-            a=decodeURI()
+            a: list[str]=decodeURI()
+            print(f"{a=}")
             a=self.comp.decompress(a)
+            print(f"{a=}")
             assert isinstance(a,list)
             assert len(a)>0
-            self.gens=a[1:]
+            self.gens: list[str] = [item.lower() for item in a[1:]]
             self.gens.sort()
             try:
                 self.seed=int(a[0:1][0])
-                print("error")
             except Exception as e:
-                print("exception !!)")
-                print(e)
+                print("exception lecture seed\n", e)
                 self.seed=0 #le 1er élément est un entier seed
             # initialiser le dict au format joueur:destinataire
             random= RandomClass.Random(self.seed)
@@ -121,7 +118,7 @@ class MainClass():
         bloque et return s'il y a des doublons
         """
         txtarea_val=str(document["noms"].value)
-        a: list[str] = [b for aa in txtarea_val.lower().replace("~","").split('\n') if (b := aa.strip())]
+        a: list[str] = [b for aa in txtarea_val.lower().split('\n') if (b := aa.strip())]
         a=self.comp.compress(a) #compression
         t: set[str]=set(a)
         doublons={}
@@ -131,7 +128,7 @@ class MainClass():
                 nb:int=a.count(item)
                 if nb>1:
                     doublons[item]=nb
-            print(f"erreur : corriger les doublons : {doublons}")
+            doc_url.clear()
             doc_url.textContent=f"erreur : corriger les doublons : {doublons}"
             return
         else:
@@ -163,16 +160,16 @@ class MainClass():
                 #case _ pass
             self.nop += 1
             
-    def reset(self,ev,p):
+    def reset(self,ev):
         "supprime les contrôles une fois et retire le cheat de la session"
         self.nop=0
-        p.unbind("click")
+        ev.target.unbind("click")
 
 
     def main(self):
 
         #dessiner la page usr
-        document <= html.P(html.BUTTON("changer de mode",).bind("click",ToggleMode.toggle),align="right",id="bout")
+        document <= html.DIV(Class="w3-container w3-right-align w3-margin-top") <= html.BUTTON("changer de mode",).bind("click",ToggleMode.toggle)
         document <= doc_usr
         document <= doc_adm
         doc_usr <= html.H1("Le tirage au sort de secret Santa par Aldoniel (Mode utilisateur)")
@@ -191,7 +188,7 @@ class MainClass():
         doc_adm <= html.H1("Mode créateur")
         p0=html.P()
         js_obj_date=window.Date.new()
-        p0<=html.INPUT(type="number",min=0, size=10, value=js_obj_date.getFullYear(),id="seed")
+        p0<=html.INPUT(  type="number",min=0, size=10, value=js_obj_date.getFullYear(),id="seed")
         p0<=html.SPAN("\tmettre un chiffre pour initialiser le générateur de hasard.")
         doc_adm<=p0
         p=html.P()
@@ -200,13 +197,11 @@ class MainClass():
         p<=html.SPAN("url",id="url")
         doc_adm <=p
         doc_adm <=html.P ("Saisir infra les prénoms des participants, un par ligne.")
-        doc_adm <= html.P (html.TEXTAREA(id="noms",rows="35", cols="80"))
-        doc_adm <=html.P ('Remarques : Le chiffre qui initialise le générateur de "hasard" permet de garantir que le tirage sera identique chez tout le monde. Ne pas utiliser ~ dans les noms (caractère réservé).')
-        p2=html.P()
-        p2<=html.BUTTON("Débloquer l'anti-triche : Je promets que c'est à bon escient.").bind("click", lambda ev, p2=p2: self.reset(ev, p2))
-        doc_adm<=p2
+        doc_adm <= html.P (html.TEXTAREA( Class="w3-left-align w3-border w3-button w3-mobile" ,id="noms",rows="35", cols="40", style="overflow-y:auto;"))
+        doc_adm <=html.P ('Remarques : Le chiffre qui initialise le générateur de "hasard" permet de garantir que le tirage sera identique chez tout le monde.')
+        doc_adm<=html.P(html.BUTTON("Débloquer l'anti-triche : Je promets que c'est à bon escient.").bind("click", self.reset))
 
-app=MainClass()
+app=MainClass()s
 app.main()
 
 #cheat code
